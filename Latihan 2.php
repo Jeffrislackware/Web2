9d15cdf87303124c2ec734c0384abfe96787fda2
Jeffri Slackware <39343259+Jeffrislackware@users.noreply.github.com>
2021-03-04 22:30:17 +0700

    Delete Latihan 2.php

--- Latihan 2.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$A = 123 ; // Variable Global
-	function Test() {
-		global $A ; // Variable Local
-		echo "Nilai A Dalam Fungsi = $A \n";
-	}
-	Test();
-	echo "Nilai A Luar Fungsi = $A \n";
-?>
